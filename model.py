6503a3edeaa2c9bfb98d3786bd9d148cf8714c02
xiaoying-xing <77709167+xiaoying-xing@users.noreply.github.com>
2024-01-22 19:05:30 -0600

    Update model.py

--- model.py
+++ model.py
@@ -17,7 +17,6 @@ class EncoderRNN(nn.Module):
         self.gru = nn.GRU(len(word_embeddings[0]), hidden_size, n_layers, dropout=self.dropout, bidirectional=True)
 
     def forward(self, input_seqs, input_lengths, hidden=None):
-        # Note: we run this all at once (over multiple batches of multiple sequences)
         embedded = self.embedding(input_seqs)
         # packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
         # outputs, hidden = self.gru(packed, hidden)
@@ -44,7 +43,6 @@ class Attn(nn.Module):
         """
 
         attn_energies = torch.bmm(hidden.transpose(0, 1), encoder_outputs.transpose(0, 1).transpose(1, 2)).squeeze(1)
-        # Normalize energies to weights in range 0 to 1, resize to 1 x B x S
         return F.softmax(attn_energies, dim=1).unsqueeze(1)
 
 
@@ -65,7 +63,6 @@ class GeneralAttn(nn.Module):
 
         attn_energies = torch.bmm(torch.matmul(hidden.transpose(0, 1), self.weight),
                                   encoder_outputs.transpose(0, 1).transpose(1, 2)).squeeze(1)
-        # Normalize energies to weights in range 0 to 1, resize to 1 x B x S
         return F.softmax(attn_energies, dim=1).unsqueeze(1)
 
 
@@ -96,9 +93,6 @@ class AttnDecoderRNN(nn.Module):
             self.attn = GeneralAttn(hidden_size)
 
     def forward(self, input_seq, last_hidden, p_encoder_outputs, pre_embedded=None):
-        # Note: we run this one step at a time
-
-        # Get the embedding of the current input word (last output word)
         if pre_embedded is None or self.condition == 'none':
             embedded = self.embedding(input_seq)
         elif self.condition == "replace":
@@ -108,25 +102,18 @@ class AttnDecoderRNN(nn.Module):
             embedded = torch.cat((embedded, pre_embedded), dim=-1)
         embedded = self.embedding_dropout(embedded)
         embedded = embedded.view(1, embedded.shape[0], embedded.shape[1])  # S=1 x B x N
-        # import pdb; pdb.set_trace()
-        # Get current hidden state from input word and last hidden state
+        
         rnn_output, hidden = self.gru(embedded, last_hidden)
 
-        # Calculate attention from current RNN state and all p_encoder outputs;
-        # apply to p_encoder outputs to get weighted average
         p_attn_weights = self.attn(rnn_output, p_encoder_outputs)
         p_context = p_attn_weights.bmm(p_encoder_outputs.transpose(0, 1))  # B x S=1 x N
 
-        # Attentional vector using the RNN hidden state and context vector
-        # concatenated together (Luong eq. 5)
         rnn_output = rnn_output.squeeze(0)  # S=1 x B x N -> B x N
 
         p_context = p_context.squeeze(1)  # B x S=1 x N -> B x N
         concat_input = torch.cat((rnn_output, p_context), 1)
         concat_output = F.tanh(self.concat(concat_input))
 
-        # Finally predict next token (Luong eq. 6, without softmax)
         output = self.out(concat_output)
 
-        # Return final output, hidden state, and attention weights (for visualization)
         return output, hidden
